cd0c25f8777ba11f8074de80b281fd4c374f166c
Daniel Amancio Vidal de Souza <souzvidal@gmail.com>
2020-03-28 14:44:16 -0300

    Finalizando o mobile da aplicação

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -65,7 +65,7 @@ export default function Incidents() {
         data={incidents}
         style={styles.incidentList}
         keyExtractor={incident => String(incident.id)}
-        // showsVerticalScrollIndicator={false}
+        showsVerticalScrollIndicator={true}
         onEndReached={loadIncidents}
         onEndReachedThreshold={0.2}
         renderItem={({ item: incident }) => (
